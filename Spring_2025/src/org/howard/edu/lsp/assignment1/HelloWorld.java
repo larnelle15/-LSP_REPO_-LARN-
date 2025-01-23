547ad0fa7d4c66b5797237e48797218fc24fd498
Larnelle15 <larnelleankunda50@gmail.com>
2025-01-23 16:22:57 -0500

    Made changes and created folders

--- Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,10 +1,10 @@
 /**
-* Name: Larnelle Ankunda
-*/
+ * Name: Larnelle Ankunda
+ */
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(&quot;Hello World!!&quot;);
-        }
-        }
+        System.out.println("Hello World!!");
+    }
+}
