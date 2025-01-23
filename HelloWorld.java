6bc1f24222b2d39a6201b0b3614db630045dac20
Larnelle15 <larnelleankunda50@gmail.com>
2025-01-23 15:58:43 -0500

    Commiting my changes to the repo

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+* Name: Larnelle Ankunda
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(&quot;Hello World!!&quot;);
+        }
+        }
